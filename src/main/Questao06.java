e3715917d11e45a82e187346ea9411e8c3c57710
Rph.cs <raphael.dev.silva@gmail.com>
2026-02-17 01:31:31 -0300

    quinta questão realizada

---
+++ src/main/Questao06.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.util.Scanner;
+/*
+ QUESTÃO 6 — Comparação com Número Anterior
+
+ O programa deve:
+
+ 1) Ler 6 números inteiros informados pelo usuário.
+ 2) Armazenar os valores em um array.
+ 3) Percorrer o array comparando cada número com o anterior.
+ 4) Contar quantas vezes o número atual é MAIOR que o anterior.
+ 5) Exibir ao final:
+
+    - A quantidade total de vezes em que houve crescimento.
+*/
+
+public class Questao06 {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int[] numeros = new int[6];
+        int numeroAnterior = 0;
+        int contagem = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            
+            System.out.format("Digite o %d numero: ", (i + 1));
+            numeros[i] = scanner.nextInt();
+            
+            if (i > 0) {
+                if (numeros[i] > numeroAnterior) {
+                    contagem++;
+                }
+            }
+            
+            numeroAnterior = numeros[i];
+        }
+    
+        System.out.format("Houve um total de %d vezes um crescimento", contagem);
+    }
+}
